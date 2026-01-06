9df643b2ebb4a6ce9ca9d60ee88e3686c98353ff
scajk <scaj@discente.ifpe.edu.br>
2026-01-06 20:53:59 -0300

    Base do projeto funcional

---
+++ src/main/java/br/com/claro/maquinavirtual/MaquinavirtualApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.claro.maquinavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaquinavirtualApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaquinavirtualApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/claro/maquinavirtual/MaquinavirtualApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.claro.maquinavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaquinavirtualApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
